8e25c6aae3583b59bb2232c2d426403dc3b9f123
leaurh <arinamehrengina@yandex.ru>
2022-07-19 18:19:56 +0300

    Solution

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,29 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+import web.service.UserServiceImpl;
+
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String userPage(Principal principal, Model model) {
+        model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
+        return "user";
+    }
+}
